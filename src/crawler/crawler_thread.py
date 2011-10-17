f7cbd6c71d6faeb6428eb0616a722e8f484204a5
Bruno Nery <bnery@isr.ist.utl.pt>
2011-10-17 17:05:48 +0100

    Implemented CheckURLAndMarkAsVisited (and test).

--- src/crawler/crawler_thread.py
+++ src/crawler/crawler_thread.py
@@ -4,20 +4,23 @@ __author__ = "Bruno Nery"
 __email__  = "brunonery@brunonery.com"
 
 from models.visitable_url import VisitableURL
+from models.visited_url import VisitedURL
 
 import threading
 
 class CrawlerThread(threading.Thread):
-    def __init__(self, database_handler, visitable_url_lock):
+    def __init__(self, database_handler, visitable_url_lock, visited_url_lock):
         """Builds a CrawlerThread instance.
 
         Arguments:
         database_handler -- the database to connect to.
         visitable_url_lock -- the thread lock for the visitable_urls table.
+        visited_url_lock -- the thread lock for the visited_urls table.
         """
         threading.Thread.__init__(self)
         self.database_handler_ = database_handler
         self.visitable_url_lock_ = visitable_url_lock
+        self.visited_url_lock_ = visited_url_lock
 
     def run(self):
         pass
@@ -38,3 +41,26 @@ class CrawlerThread(threading.Thread):
             session.commit()
         self.visitable_url_lock_.release()
         return the_url
+
+    def CheckURLAndMarkAsVisited(self, url):
+        """Check if the URL has already been visited and mark it as visited.
+
+        Arguments:
+        url -- the URL to be checked.
+        
+        Returns:
+        True if the URL was already visited. False otherwise.
+        """
+        self.visited_url_lock_.acquire()
+        the_url = VisitedURL(url)
+        session = self.database_handler_.CreateSession()
+        query = session.query(VisitedURL)
+        result = query.filter(VisitedURL.url_md5 == the_url.url_md5)
+        if result.count() == 0:
+            url_is_visited = False
+            session.add(the_url)
+            session.commit()
+        else:
+            url_is_visited = True
+        self.visited_url_lock_.release()
+        return url_is_visited
--- src/crawler/crawler_thread_test.py
+++ src/crawler/crawler_thread_test.py
@@ -6,6 +6,7 @@ __email__  = "brunonery@brunonery.com"
 from common.database_handler import DatabaseHandler
 from crawler_thread import CrawlerThread
 from models.visitable_url import VisitableURL
+from models.visited_url import VisitedURL
 
 import threading
 import unittest
@@ -22,7 +23,8 @@ class CrawlerThreadTest(unittest.TestCase):
         session.add(VisitableURL('http://www.google.com/', 1))
         session.commit()
          # Test pop.
-        crawler_thread = CrawlerThread(database_handler, visitable_url_lock)
+        crawler_thread = CrawlerThread(
+            database_handler, visitable_url_lock, None)
         the_url = crawler_thread.PopVisitableURL()
         assert the_url.url == 'http://www.google.com/'
         assert the_url.priority == 1
@@ -33,3 +35,20 @@ class CrawlerThreadTest(unittest.TestCase):
         # No more pops.
         the_url = crawler_thread.PopVisitableURL()
         assert the_url == None
+
+    def testCheckURLAndMarkAsVisitedWorks(self):
+        # Create test database and lock.
+        database_handler = DatabaseHandler('sqlite:///:memory:')
+        database_handler.Init()
+        visited_url_lock = threading.Lock()
+        # Populate the test database.
+        session = database_handler.CreateSession()
+        session.add(VisitedURL('http://www.microsoft.com/'))
+        session.add(VisitedURL('http://www.google.com/'))
+        session.commit()
+         # Test check.
+        crawler_thread = CrawlerThread(database_handler, None, visited_url_lock)
+        assert crawler_thread.CheckURLAndMarkAsVisited('http://www.google.com/') 
+        assert crawler_thread.CheckURLAndMarkAsVisited('http://www.microsoft.com/')
+        assert not crawler_thread.CheckURLAndMarkAsVisited('http://www.facebook.com/')
+        assert crawler_thread.CheckURLAndMarkAsVisited('http://www.facebook.com/')
