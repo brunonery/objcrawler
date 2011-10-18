1b6f9d0477d4528adc44e0786b9d3167157ffaae
Bruno Nery <bnery@isr.ist.utl.pt>
2011-10-18 11:00:47 +0100

    Added number of threads as crawler parameter.

--- src/crawler/crawler.py
+++ src/crawler/crawler.py
@@ -13,23 +13,25 @@ import threading
 parser = argparse.ArgumentParser(
     description='Crawls the web looking for 3D object models.')
 parser.add_argument('--config', action='store', type=str)
-# TODO(brunonery): add argument for number of threads (with default).
+parser.add_argument('--instances', action='store', type=int, default=10)
 
 if __name__ == "__main__":
     # TODO(brunonery): verify arguments and fail gracefully if necessary.
     args = parser.parse_args()
     config = CrawlerConfig(open(args.config))
+    # Prepare database and locks.
     database_handler = DatabaseHandler(config.database_address())
     database_handler.Init()
     visitable_url_lock = threading.Lock()
     visited_url_lock = threading.Lock()
+    # Start all threads.
     thread_list = []
-    for i in range(10):
+    for i in range(args.instances):
         current_thread = CrawlerThread(database_handler,
                                        visitable_url_lock,
                                        visited_url_lock)
         thread_list.append(current_thread)
         current_thread.start()
-        
+    # Wait for all threads to finish.
     for thread in thread_list:
         thread.join()
