f6e5d7615b345977b5525c3b9d8c385e533cea9f
Bruno Nery <bnery@isr.ist.utl.pt>
2011-10-20 18:52:28 +0100

    Added max_results capability to GoogleSearch (and tests).

--- src/seeder/google_helper_test.py
+++ src/seeder/google_helper_test.py
@@ -8,12 +8,24 @@ from google_helper import GoogleSearch
 import unittest
 
 class GoogleSearchTest(unittest.TestCase):
+    # Attention: these test is flaky for a set of reasons: the number of
+    # requests per day is limited, and the first 10 results for Google might not
+    # contain 'http://www.google.com' (but this is less probable).
     def testGoogleSearchWorks(self):
-        # Attention: this test is flaky for a set of reasons: the number of
-        # requests per day is limited, and the first 10 results for Google
-        # might not contain 'http://www.google.com' (but this is less probable).
         search_results = GoogleSearch('AIzaSyCrGh4R7a7-ayRQyh7nXPwuBy6O7F0VqRM',
                                       '017513622067795982245:_iwk5xznrk0',
                                       'Google')
         assert len(filter(lambda r: r.link == 'http://www.google.com/',
                           search_results)) > 0
+
+    def testGoogleSearchWithMaxResultsWork(self):
+        search_results = GoogleSearch('AIzaSyCrGh4R7a7-ayRQyh7nXPwuBy6O7F0VqRM',
+                                      '017513622067795982245:_iwk5xznrk0',
+                                      'Google',
+                                      max_results=20)
+        assert len(search_results) == 20
+        search_results = GoogleSearch('AIzaSyCrGh4R7a7-ayRQyh7nXPwuBy6O7F0VqRM',
+                                      '017513622067795982245:_iwk5xznrk0',
+                                      'Google',
+                                      max_results=13)
+        assert len(search_results) == 13
