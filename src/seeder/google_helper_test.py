9590d78d72785c527988f00288d43a3a3c94129c
Bruno Nery <brunonery@brunonery.com>
2011-10-27 17:42:52 +0100

    Changed GoogleSearchTest tests to replay data obtained from Google.

--- src/seeder/google_helper_test.py
+++ src/seeder/google_helper_test.py
@@ -5,27 +5,49 @@ __email__  = "brunonery@brunonery.com"
 
 from google_helper import GoogleSearch
 
+import mock
+import pickle
+import testfixtures
 import unittest
 
+class GoogleApiMock():
+    def __init__(self):
+        self.service = mock.Mock()
+        mock_search_engine = mock.Mock()
+        mock_search_engine.list = self.mock_list
+        self.cse = mock.Mock(return_value=mock_search_engine)
+
+    def mock_list(self, q, cref, start, num):
+        filename = ('test/data/googlesearch_%s_%d_%d.p' % (q, start, num))
+        with open(filename, 'rb') as f:
+            search_result = pickle.load(f)
+        response = mock.Mock()
+        response.execute = mock.Mock(return_value=search_result)
+        return response
+
 class GoogleSearchTest(unittest.TestCase):
-    # Attention: these test is flaky for a set of reasons: the number of
-    # requests per day is limited, and the first 10 results for Google might not
-    # contain 'http://www.google.com' (but this is less probable).
     def testGoogleSearchWorks(self):
-        search_results = GoogleSearch('AIzaSyCrGh4R7a7-ayRQyh7nXPwuBy6O7F0VqRM',
-                                      '017513622067795982245:_iwk5xznrk0',
-                                      'Google')
-        assert len(filter(lambda r: r.link == 'http://www.google.com/',
-                          search_results)) > 0
+        with testfixtures.Replacer() as r:
+            r.replace('apiclient.discovery.build', mock.Mock(return_value=GoogleApiMock()))
+            search_results = GoogleSearch(
+                'AIzaSyCrGh4R7a7-ayRQyh7nXPwuBy6O7F0VqRM',
+                '017513622067795982245:_iwk5xznrk0',
+                'google')
+            assert len(filter(lambda r: r.link == 'http://www.google.com/',
+                              search_results)) > 0
 
     def testGoogleSearchWithMaxResultsWork(self):
-        search_results = GoogleSearch('AIzaSyCrGh4R7a7-ayRQyh7nXPwuBy6O7F0VqRM',
-                                      '017513622067795982245:_iwk5xznrk0',
-                                      'Google',
-                                      max_results=20)
-        assert len(search_results) <= 20
-        search_results = GoogleSearch('AIzaSyCrGh4R7a7-ayRQyh7nXPwuBy6O7F0VqRM',
-                                      '017513622067795982245:_iwk5xznrk0',
-                                      'Google',
-                                      max_results=13)
-        assert len(search_results) <= 13
+        with testfixtures.Replacer() as r:
+            r.replace('apiclient.discovery.build', mock.Mock(return_value=GoogleApiMock()))
+            search_results = GoogleSearch(
+                'AIzaSyCrGh4R7a7-ayRQyh7nXPwuBy6O7F0VqRM',
+                '017513622067795982245:_iwk5xznrk0',
+                'google',
+                max_results=20)
+            assert len(search_results) <= 20
+            search_results = GoogleSearch(
+                'AIzaSyCrGh4R7a7-ayRQyh7nXPwuBy6O7F0VqRM',
+                '017513622067795982245:_iwk5xznrk0',
+                'google',
+                max_results=13)
+            assert len(search_results) <= 13
